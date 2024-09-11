9c486b15754c0e75a1ad7919512ce30d0060a87e
Elena-Martinez-cloud <elena.martinez-elejarza@g.austincc.edu>
2024-09-11 18:05:02 +0000

    homework 2

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,7 @@
-#
+def test_config():
+    return True
+
+def get_options_ratio():
+
+
+    
--- tests/homework/c_decisions/tests_decisions.py
+++ tests/homework/c_decisions/tests_decisions.py
@@ -1,1 +1,8 @@
-#
+import unittest
+from src.homework.c_decisions.decisions import get_options_ratio
+from src.homework.c_decisions.decisions import get_faculty_rating
+
+class Test_config(unittest.TestCase):
+
+    def test_get_options_ratio (self):
+        
